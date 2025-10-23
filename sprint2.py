d628dde803ee3afb844d688e84892170563cf3c4
Juanpabloflorez <juflorezru@unal.edu.co>
2025-10-23 15:58:28 -0500

    Update sprint2.py

--- sprint2.py
+++ sprint2.py
@@ -7,18 +7,10 @@ firebase_admin.initialize_app(cred, {"databaseURL": "https://proyecto-poo-b05c4-
 ref = db.reference("/")
 usuarios = db.reference("Usuarios")
 
-def crearTarea():
-    ref.child("Usuarios").child("Jairo").set({"Password": "4321"})
-    ref.child("Usuarios").child("Jairo").child("Tareas").child("1").set({"Titulo": "POO","importanciaTarea": 2})
-
-def proponerImportancia(user):
-    get = usuarios.get()
-    tarea=input("Ingrese el numero de su tarea: ")
-
-    importancia=int(input("Ingrese un valor del 1 al 3 para establecer la importancia de la tarea: "))
-    while importancia != 1 and importancia != 2 and importancia != 3:
-        importancia=input("Nivel de importancia incoherente (rango de 1 a 3)")
-    ref.child("Usuarios").child(user).child("Tareas").child(tarea).set({"importanciaTarea": importancia})
+def crearUsuario():
+    user = input("Crea el nombre de tu nueva cuenta: ")
+    password = input("Crea una contraseña para tu nueva cuenta: ")
+    ref.child("Usuarios").child(user).set({"Password: ": password})
 
 def login():
     user=input("Ingrese su usuario: ")
@@ -34,17 +26,66 @@ def login():
             password=input("Contraseña incorrecta, intente nuevamente")
         leerTareas(user)
 
+
+def agregarTarea(user):
+    tarea = input("Ingrese el nombre de su nueva tarea: ")
+    importancia = int(input("Ingrese un valor del 1 al 3 para establecer la importancia de la tarea (1 minima, 2 medio, 3 maxima): "))
+    while importancia != 1 and importancia != 2 and importancia != 3:
+        importancia=int(input("Nivel de importancia incoherente (rango de 1 a 3): "))
+    categoria = int(input("Ingrese un valor del 1 al 3 para establecer la categoria de la tarea (1 personal, 2 estudios, 3 trabajo): "))
+
+    ref.child("Usuarios").child(user).child("Tareas").child(tarea).set({"importancia de la Tarea": importancia, "Categoria: ": categoria})
+
+
+def actualizarTarea(user):
+    opcion=int(input("Ingrese 1 para editar el nombre de la tarea, 2 para editar la importancia, 3 para editar la categoria: "))
+
+    while opcion != 1 and opcion != 2 and opcion != 3:
+        opcion=int(input("Opcion no valida. Ingrese 1 para editar el nombre de la tarea, 2 para editar la importancia: "))
+
+    if opcion == 1:
+        tarea=input("Ingrese el nombre original de la tarea: ")
+        nombreNuevo = input("Ingrese el nuevo nombre: ")
+        tareaOriginal = ref.child("Usuarios").child(user).child("Tareas").child(tarea).get()
+        ref.child("Usuarios").child(user).child("Tareas").child(nombreNuevo).set(tareaOriginal)
+        tareaOriginal = ref.child("Usuarios").child(user).child("Tareas").child(tarea).delete()
+
+    elif opcion == 2:
+        importancia=int(input("Ingrese un valor del 1 al 3 para establecer la importancia de la tarea: "))
+        while importancia != 1 and importancia != 2 and importancia != 3:
+            importancia=int(input("Nivel de importancia incoherente (rango de 1 a 3): "))
+        ref.child("Usuarios").child(user).child("Tareas").child(tarea).set({"importancia de la Tarea": importancia})
+
+    elif opcion == 3:
+        categoria = int(input("Ingrese un valor del 1 al 3 para establecer la categoria de la tarea (1 personal, 2 estudios, 3 trabajo): "))
+
+        while categoria != 1 and opcion != 2 and opcion != 3:
+            categoria=int(input("Opcion no valida. Ingrese un valor del 1 al 3 para establecer la categoria de la tarea (1 personal, 2 estudios, 3 trabajo): "))
+
+        ref.child("Usuarios").child(user).child("Tareas").child(tarea).update({"Categoria: ": categoria})
+        
+
+
+
+def eliminarTarea(user):
+    tarea=input("Ingrese el nombre original de la tarea: ")
+    ref.child("Usuarios").child(user).child("Tareas").child(tarea).delete()
+    print(f"Tarea: ",tarea,"ha sido eliminada")
+        
+
 def leerTareas(user):
-    tarea=int(input("Ingrese una tarea: "))
-    get = usuarios.get()
-    datos = get[user]["Tareas"][tarea]
-    print(f"Tarea {tarea}:")
-    for clave, valor in datos.items():
-        print(f"  {clave}: {valor}")
+    tareas=ref.child("Usuarios").child(user).child("Tareas").get()
+
+    if tareas:
+        for clave, datos in tareas.items():
+            print(f"Tarea ID: {clave}")
+            print(f"Datos: {datos}")
+    else:
+        print("No hay tareas registradas.")
 
 
 def main():
-    proponerImportancia("Jairo")
+    agregarTarea("Jairo")
 
 if __name__ == "__main__":
     main()
